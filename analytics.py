e09ab6bc6f9e360764f7e9d408e95a03b38768dd
jpayvazian <jpayvazian@wpi.edu>
2022-11-18 15:51:23 -0500

    refactor

--- analytics.py
+++ analytics.py
@@ -5,43 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 PLOT_DIR = 'plots'
+REVIEW_FILES = ['one_star.json', 'two_star.json', 'three_star.json', 'four_star.json', 'five_star.json']
+
 if __name__ == "__main__":
-    # Separate reviews by star rating
-    reviews_1star = utils.load_data('one_star.json')
-    reviews_2star = utils.load_data('two_star.json')
-    reviews_3star = utils.load_data('three_star.json')
-    reviews_4star = utils.load_data('four_star.json')
-    reviews_5star = utils.load_data('five_star.json')
-
-    # Tokenize reviews
-    tokenizer1 = Tokenizer()
-    tokenizer2 = Tokenizer()
-    tokenizer3 = Tokenizer()
-    tokenizer4 = Tokenizer()
-    tokenizer5 = Tokenizer()
-
+    # Metrics for data exploration
     avg_review_len = []
     vocab_size = []
-    # Calculate vocabulary size and truncate/pad reviews
-    tokenizer1.fit_on_texts(reviews_1star['text'])
-    avg_review_len.append(np.mean([len(s) for s in reviews_1star['sequences']]))
-    vocab_size.append(len(tokenizer1.word_index) + 1)
 
-    tokenizer2.fit_on_texts(reviews_2star['text'])
-    avg_review_len.append(np.mean([len(s) for s in reviews_2star['sequences']]))
-    vocab_size.append(len(tokenizer2.word_index) + 1)
+    for file in REVIEW_FILES:
+        reviews = utils.load_data(file)
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts(reviews['text'])
 
-    tokenizer3.fit_on_texts(reviews_3star['text'])
-    avg_review_len.append(np.mean([len(s) for s in reviews_3star['sequences']]))
-    vocab_size.append(len(tokenizer3.word_index) + 1)
-
-    tokenizer4.fit_on_texts(reviews_4star['text'])
-    avg_review_len.append(np.mean([len(s) for s in reviews_4star['sequences']]))
-    vocab_size.append(len(tokenizer4.word_index) + 1)
-
-    tokenizer5.fit_on_texts(reviews_5star['text'])
-    avg_review_len.append(np.mean([len(s) for s in reviews_5star['sequences']]))
-    vocab_size.append(len(tokenizer5.word_index) + 1)
+        # Calculate vocabulary size and average review length
+        avg_review_len.append(np.mean([len(w.split()) for w in reviews['text']]))
+        vocab_size.append(len(tokenizer.word_index) + 1)
 
     # Make graphs with data
     plt.bar([1,2,3,4,5], vocab_size)
@@ -49,7 +27,7 @@ if __name__ == "__main__":
     plt.xlabel("Stars")
     plt.ylabel("Vocabulary size")
     plt.savefig(os.path.join(PLOT_DIR, 'vocab_size.png'))
-
+    plt.clf()
     plt.bar([1,2,3,4,5], avg_review_len)
     plt.title('Yelp reviews: Average length of review')
     plt.xlabel("Stars")
