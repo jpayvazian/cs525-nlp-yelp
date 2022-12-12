2d5fe8e683e7dd929d1689193e1862d2eaf606ae
Dillon McCarthy <dillonmccarthy423@gmail.com>
2022-12-12 11:39:37 -0500

    improved machine evaluation

--- analytics.py
+++ analytics.py
@@ -1,6 +1,7 @@
 from utils import *
 from keras.preprocessing.text import Tokenizer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.manifold import TSNE
 from sklearn.ensemble import RandomForestClassifier
@@ -69,6 +70,7 @@ def LSA(star_num):
     plt.title(f't-SNE Clustering of LSA: Real vs Generated YELP Reviews {star_num+1} stars')
     plt.savefig(os.path.join(PLOT_DIR, f'LSA_{star_num}.png'))
 
+# Analyzes separability of real and fake data using a binary classifier
 def simple_machine_evaluation(star_num):
     # Load data for specified star rating
     real = load_data(REVIEW_FILES[star_num])['text'].to_list()[:GEN_SIZE]
@@ -82,7 +84,7 @@ def simple_machine_evaluation(star_num):
     both = pd.concat([fake, real])
 
     # Use Count Vectorizer to turn text in numerical data
-    cv = CountVectorizer()
+    cv = TfidfVectorizer()
     vec = cv.fit_transform(both['text'])
 
     train_x, test_x, train_y, test_y = train_test_split(vec, both['label'], test_size=.3)
@@ -93,9 +95,12 @@ def simple_machine_evaluation(star_num):
 
     # Evaluate classifier
     pred = model_real.predict(test_x)
+    print(f'Separability of {star_num+1}-star data')
     print(confusion_matrix(test_y, pred))
     print('Accuracy\t' + str(model_real.score(test_x, test_y)))
 
+# Analyzes distribution of classes for real and fake data.  A classifier trained on real data to classify
+# a star rating 1-5 should (ideally) perform equally well on real and fake data, and vice versa
 def class_machine_evaluation():
     # Load data for specified star rating
     all_real_reviews = []
@@ -115,7 +120,7 @@ def class_machine_evaluation():
     combined_data = all_real_reviews + all_fake_reviews
 
     # Use Count Vectorizer to turn text in numerical data
-    cv = CountVectorizer()
+    cv = TfidfVectorizer()
     vec = cv.fit_transform(combined_data)
     real_vec = vec[:len(all_real_reviews)]
     fake_vec = vec[len(all_real_reviews):]
@@ -132,24 +137,30 @@ def class_machine_evaluation():
     model_fake.fit(fake_train_x, fake_train_y)
 
     # Evaluate classifier trained on real data
+    print('Evaluating classifier trained on real data')
     # Real Data Evaluatation
     pred = model_real.predict(real_test_x)
+    print('Real Data Evaluation')
     print(confusion_matrix(real_test_y, pred))
     print('Accuracy\t' + str(model_real.score(real_test_x, real_test_y)))
 
     # Fake Data Evaluatation
     pred = model_real.predict(fake_test_x)
+    print('Fake Data Evaluation')
     print(confusion_matrix(fake_test_y, pred))
     print('Accuracy\t' + str(model_real.score(fake_test_x, fake_test_y)))
 
     # Evaluate classifier trained on fake data
+    print('Evaluating classifier trained on fake data')
     # Real Data Evaluatation
     pred = model_fake.predict(real_test_x)
+    print('Real Data Evaluation')
     print(confusion_matrix(real_test_y, pred))
     print('Accuracy\t' + str(model_fake.score(real_test_x, real_test_y)))
 
     # Fake Data Evaluatation
     pred = model_fake.predict(fake_test_x)
+    print('Fake Data Evaluation')
     print(confusion_matrix(fake_test_y, pred))
     print('Accuracy\t' + str(model_fake.score(fake_test_x, fake_test_y)))
 
@@ -205,7 +216,8 @@ if __name__ == "__main__":
     star_num = 0        # The star rating 1-5 indexed at 0-4
     # data_exploration()
     # LSA(0)
-    # simple_machine_evaluation(0)
+    for i in range(5):
+        simple_machine_evaluation(i)
     # class_machine_evaluation()
     # gpt_detector()
     # word_frequencies(star_num)
