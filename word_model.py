e48686dfeb074bb9079d4555cacd1bce3fa25baa
Dillon McCarthy <dillonmccarthy423@gmail.com>
2022-12-12 11:09:43 -0500

    improve model weights and load/save model

--- word_model.py
+++ word_model.py
@@ -57,10 +57,10 @@ def create_word_model(reviews, star):
     # Create labels from last word/char of sequence
     y = to_categorical(np.array(y), num_classes=vocab_size)
 
-    # Create callback to save training checkpoint
-    checkpoint_path = f'checkpoint/{star}_word_model.ckpt'
-    checkpoint_dir = os.path.dirname(checkpoint_path)
-    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
+    # # Create callback to save training checkpoint
+    # checkpoint_path = f'checkpoint/{star}_word_model.ckpt'
+    # checkpoint_dir = os.path.dirname(checkpoint_path)
+    # cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
 
     # Create and train LSTM Model
     model = Sequential([
@@ -73,13 +73,36 @@ def create_word_model(reviews, star):
     ])
     model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-    # Load from checkpoint if available
-    try:
-        model.load_weights(checkpoint_path)
-        print("Loading from checkpoint...")
-    except:
-        print('Creating new model...')
-    
-    model.fit(np.array(x), y, epochs=EPOCHS, batch_size=BATCH_SIZE, callbacks=[cp_callback])
+    model.load_weights('./checkpoint/word_model/weights.h5')
+
+    # model.fit(np.array(x), y, epochs=EPOCHS, batch_size=BATCH_SIZE)
+
+    # model.save_weights('./checkpoint/word_model/weights.h5')
 
     return model, tokenizer
+
+if __name__ == "__main__":
+    star = int(input("Enter star rating for review (1-5)"))
+
+    # Names for data text file and model save dir
+    file_name = os.path.join(DATA_DIR, TEXT_FILES[star - 1])
+    run_name = f"{star}star_word_model_{EPOCHS}epoch"
+    data_file_name = os.path.join(DATA_DIR, f'{star}star_word_model.json')
+
+    # Load reviews
+    reviews = load_data(REVIEW_FILES[star-1])['text'].to_list()[:GEN_SIZE]
+
+    # Finetune/load model
+    model, tokenizer = create_word_model(reviews, star)
+
+    # Generate reviews with same length and prefix as real ones
+    fake_reviews = []
+    for i in range(GEN_SIZE):
+        # Split review into list of words
+        review_words = reviews[i].split()
+        fake_reviews.append(generate_review(model, tokenizer, ''.join(review_words[:PREFIX_SIZE]), len(review_words)))
+        print(f'\r{i+1}/{GEN_SIZE}', end= '')
+
+    with open(data_file_name, 'w', encoding='utf8') as f:
+        f.write(json.dumps(fake_reviews))
+
