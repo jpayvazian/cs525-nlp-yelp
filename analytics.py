2b59a6ff35a1220e00b97008d7448b2106bf74b6
Dillon McCarthy <dillonmccarthy423@gmail.com>
2022-12-13 10:01:26 -0500

    bleu and rouge score

--- analytics.py
+++ analytics.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 from transformers import TFAutoModel, AutoTokenizer, pipeline
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
+from nltk.translate.bleu_score import SmoothingFunction, corpus_bleu, sentence_bleu
+import itertools
 
 def data_exploration():
     # Metrics for data exploration
@@ -297,6 +299,103 @@ def word_frequencies(star):
     real_df.to_csv(f'./plots/real_{star}star_frequencies.csv')
     fake_df.to_csv(f'./plots/fake_{star}star_frequencies.csv')
 
+def bleu_score(star):
+    # Load data for specified star rating
+    real = load_data(REVIEW_FILES[star])['text'].to_list()[:GEN_SIZE]
+    fake = load_json(FAKE_REVIEW_FILES[star])
+
+    ref_bleu = []
+    gen_bleu = []
+    for l in fake:
+        gen_bleu.append(l.split())
+    for i,l in enumerate(real):
+        ref_bleu.append([l.split()])
+
+    cc = SmoothingFunction()
+    score_bleu = corpus_bleu(ref_bleu, gen_bleu, weights=(0, 1, 0, 0), smoothing_function=cc.method4)
+    print(f'BLEU:\t{score_bleu}')
+
+    return score_bleu
+
+#rouge scores for a reference/generated sentence pair
+#source google seq2seq source code.
+#supporting function
+def _split_into_words(sentences):
+    """Splits multiple sentences into words and flattens the result"""
+    return list(itertools.chain(*[_.split(" ") for _ in sentences]))
+
+#supporting function
+def _get_word_ngrams(n, sentences):
+    """Calculates word n-grams for multiple sentences.
+    """
+    assert len(sentences) > 0
+    assert n > 0
+
+    words = _split_into_words(sentences)
+    return _get_ngrams(n, words)
+
+#supporting function
+def _get_ngrams(n, text):
+    """Calcualtes n-grams.
+    Args:
+    n: which n-grams to calculate
+    text: An array of tokens
+    Returns:
+    A set of n-grams
+    """
+    ngram_set = set()
+    text_length = len(text)
+    max_index_ngram_start = text_length - n
+    for i in range(max_index_ngram_start + 1):
+        ngram_set.add(tuple(text[i:i + n]))
+    return ngram_set
+
+def rouge_n(star, n=2):
+    """
+    Computes ROUGE-N of two text collections of sentences.
+    Source: http://research.microsoft.com/en-us/um/people/cyl/download/
+    papers/rouge-working-note-v1.3.1.pdf
+    Args:
+    evaluated_sentences: The sentences that have been picked by the summarizer
+    reference_sentences: The sentences from the referene set
+    n: Size of ngram.  Defaults to 2.
+    Returns:
+    recall rouge score(float)
+    Raises:
+    ValueError: raises exception if a param has len <= 0
+    """
+    # Load data for specified star rating
+    reference_sentences = load_data(REVIEW_FILES[star])['text'].to_list()[:GEN_SIZE]
+    evaluated_sentences = load_json(FAKE_REVIEW_FILES[star])
+
+    if len(evaluated_sentences) <= 0 or len(reference_sentences) <= 0:
+        raise ValueError("Collections must contain at least 1 sentence.")
+
+    evaluated_ngrams = _get_word_ngrams(n, evaluated_sentences)
+    reference_ngrams = _get_word_ngrams(n, reference_sentences)
+    reference_count = len(reference_ngrams)
+    evaluated_count = len(evaluated_ngrams)
+
+    # Gets the overlapping ngrams between evaluated and reference
+    overlapping_ngrams = evaluated_ngrams.intersection(reference_ngrams)
+    overlapping_count = len(overlapping_ngrams)
+
+    # Handle edge case. This isn't mathematically correct, but it's good enough
+    if evaluated_count == 0:
+        precision = 0.0
+    else:
+        precision = overlapping_count / evaluated_count
+
+    if reference_count == 0:
+        recall = 0.0
+    else:
+        recall = overlapping_count / reference_count
+
+    f1_score = 2.0 * ((precision * recall) / (precision + recall + 1e-8))
+    print(f'ROUGE:\t{recall}')
+
+    return recall
+
 if __name__ == "__main__":
 
     star_num = 0        # The star rating 1-5 indexed at 0-4
@@ -307,6 +406,8 @@ if __name__ == "__main__":
     # sentiment_machine_evaluation()
     # gpt_detector()
     # word_frequencies(star_num)
+    bleu_score(star_num)
+    rouge_n(star_num)
 
     # Run machine eval multiple times and save to csv for averaging classifier accuracy
     # reps = 20
